2c1e9252d394b31ba3bde9876c112f84faf1ee06
Patricio Paez <pp@pp.com.mx>
2023-11-24 10:51:09 -0600

    Agregar acceso a la base de datos
    Basado en "Using SQLite 3 in Flask" en la sección
    "Patterns for Flask" de la documentación.
    
    Usar un archivo de configuración predeterminada para
    definir la ruta a la base de datos, basado en
    "Configuring from Python Files" de la sección
    "Configuration Handling" de la documentación de Flask.

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
+import sqlite3
 from flask import Flask
 from flask import render_template
+from flask import g
 
 
 app = Flask(__name__)
+app.config.from_object("config")
+DATABASE = app.config["DATABASE"]
+
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
 
 @app.route("/")
 def lista():
-    return render_template("lista.html")
+    cur = get_db().cursor()
+    cmd = "SELECT numero, cambios, tema, resumen, tags, autor, estado FROM sintema"
+    cur.execute(cmd)
+    records = cur.fetchall()
+    editar = {r["numero"]: r["estado"] == "" for r in records}
+    tags = {r["numero"]: r["tags"].split("|") for r in records}
+    cmd = "SELECT nombre FROM areas"
+    cur.execute(cmd)
+    areas = cur.fetchall()
+    return render_template(
+        "lista.html", records=records, editar=editar, tags=tags, areas=areas
+    )

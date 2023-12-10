bb18285ec21495eb86992609c9aa4fd513001183
Patricio Paez <pp@pp.com.mx>
2023-12-09 20:32:36 -0600

    Agregar datos de prueba para la base de datos
    Basado en "Setup and Fixtures" en la sección
    "Test Coverage" del Tutorial en la User's Guide
    de Flask.

---
+++ tests/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO sintema (legislatura, numero, cambios, tema, resumen, tags,
+comentario, autor, estado)
+VALUES ('LXIII', '1', 'cambios', 'tema1', 'resumen1', 'tag1|tag2', 'comentario1',
+'autor1', 'estado1');
+
+INSERT INTO areas (nombre)
+VALUES ('area1');
+
+INSERT INTO usuarios (usuario)
+VALUES ('autor1');

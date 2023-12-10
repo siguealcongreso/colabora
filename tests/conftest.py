488099e54090e3151a83443755f00dbe215c9161
Patricio Paez <pp@pp.com.mx>
2023-12-09 20:26:42 -0600

    Agregar fixtures para pruebas
    Basado en "Setup and Fixtures" en la sección
    "Test Coverage" del Tutorial en la User's Guide
    de Flask.

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import os
+import tempfile
+
+import pytest
+from app import app as appli, get_db, init_db
+
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode()
+
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+    print(db_fd, db_path)
+
+    appli.config['DATABASE'] = db_path
+    appli.testing = True
+    with appli.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield appli
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()

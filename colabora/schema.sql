9c358081401142a65f879e9379c650b66bb6c2ed
Patricio Paez <pp@pp.com.mx>
2023-12-11 18:15:32 -0600

    Agregar inicialización de la base de datos

---
+++ colabora/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS sintema;
+DROP TABLE IF EXISTS areas;
+DROP TABLE IF EXISTS usuarios;
+
+CREATE TABLE sintema (
+    id INTEGER PRIMARY KEY,
+    legislatura TEXT NOT NULL,
+    numero TEXT NOT NULL,
+    cambios TEXT DEFAULT '',
+    tema TEXT DEFAULT '',
+    resumen TEXT DEFAULT '',
+    tags TEXT DEFAULT '',
+    comentario TEXT DEFAULT '',
+    autor TEXT DEFAULT '',
+    estado TEXT DEFAULT ''
+    );
+
+CREATE TABLE areas (
+    id INTEGER PRIMARY KEY,
+    nombre TEXT NOT NULL
+    );
+
+CREATE TABLE usuarios (
+    id INTEGER PRIMARY KEY,
+    usuario TEXT DEFAULT '',
+    contrasena TEXT DEFAULT ''
+    );

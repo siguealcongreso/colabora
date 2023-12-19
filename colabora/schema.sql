87d917087e678c95a09d4b82641501ee09109920
Patricio Paez <pp@pp.com.mx>
2023-12-19 08:52:28 -0600

    Actualizar el esquema de la base de datos
    Agregar las tablas estado y legislatura.
    
    Agregar la columna estado_id a la tabla iniciativas
    y renombrar la columna legislatura a legislatura_id.
    
    Remover las columnas tags y autor, agregar llave
    primaria, hacer la columna numero entera en la tabla
    iniciativas.
    
    Renombrar la columna area_id en areas y usuario_id
    en usuarios.
    
    Agregar la tabla clasificacion que refiere a las tablas
    iniciativas y areas.  Agregar la tabla asignacion que
    refiere a las tablas iniciativas y usuarios.

--- colabora/schema.sql
+++ colabora/schema.sql
@@ -1,28 +1,59 @@
+DROP TABLE IF EXISTS clasificacion;
+DROP TABLE IF EXISTS asignacion;
+DROP TABLE IF EXISTS estado;
+DROP TABLE IF EXISTS legislatura;
 DROP TABLE IF EXISTS iniciativas;
 DROP TABLE IF EXISTS areas;
 DROP TABLE IF EXISTS usuarios;
 
+CREATE TABLE estado (
+    estado_id INTEGER PRIMARY KEY,
+    nombre TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE legislatura (
+    legislatura_id INTEGER PRIMARY KEY,
+    nombre TEXT NOT NULL UNIQUE
+);
+
 CREATE TABLE iniciativas (
-    id INTEGER PRIMARY KEY,
-    legislatura TEXT NOT NULL,
-    numero TEXT NOT NULL UNIQUE,
+    estado_id INTEGER NOT NULL,
+    legislatura_id INTEGER NOT NULL,
+    numero INTEGER NOT NULL,
     cambios TEXT DEFAULT '',
     tema TEXT DEFAULT '',
     resumen TEXT DEFAULT '',
-    tags TEXT DEFAULT '',
     comentario TEXT DEFAULT '',
-    autor TEXT DEFAULT '',
-    estado TEXT DEFAULT ''
+    estado TEXT DEFAULT '',
+    PRIMARY KEY (estado_id, legislatura_id, numero)
     );
 
 CREATE TABLE areas (
-    id INTEGER PRIMARY KEY,
+    area_id INTEGER NOT NULL PRIMARY KEY,
     nombre TEXT NOT NULL UNIQUE
     );
 
 CREATE TABLE usuarios (
-    id INTEGER PRIMARY KEY,
+    usuario_id INTEGER PRIMARY KEY,
     usuario TEXT NOT NULL UNIQUE,
     contrasena TEXT DEFAULT '',
     rol TEXT DEFAULT ''
     );
+
+CREATE TABLE clasificacion (
+    estado_id INTEGER NOT NULL,
+    legislatura_id INTEGER NOT NULL,
+    numero INTEGER NOT NULL,
+    area_id INTEGER NOT NULL REFERENCES areas(area_id),
+    PRIMARY KEY (estado_id, legislatura_id, numero, area_id),
+    FOREIGN KEY (estado_id, legislatura_id, numero) REFERENCES iniciativas
+);
+
+CREATE TABLE asignacion (
+    estado_id INTEGER NOT NULL,
+    legislatura_id INTEGER NOT NULL,
+    numero INTEGER NOT NULL,
+    usuario_id INTEGER NOT NULL REFERENCES usuarios(usuario_id),
+    PRIMARY KEY (estado_id, legislatura_id, numero, usuario_id),
+    FOREIGN KEY (estado_id, legislatura_id, numero) REFERENCES iniciativas
+);

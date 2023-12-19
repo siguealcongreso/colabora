198243f90197fd02cb6ffe061b0082a0c1add57e
Patricio Paez <pp@pp.com.mx>
2023-12-19 09:22:56 -0600

    Activar límites por llaves foráneas

--- colabora/db.py
+++ colabora/db.py
@@ -8,6 +8,7 @@ def get_db():
     db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(app.config["DATABASE"])
+        db.execute('PRAGMA foreign_keys = ON')
     db.row_factory = sqlite3.Row
     return db
 

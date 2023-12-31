220e3b98e3a3f9e19f5c8fcb630ee9e4b6472d11
Patricio Paez <pp@pp.com.mx>
2023-12-31 13:14:52 -0600

    Usar secure headers
    Basado en el ejemplo para Flask en "Supported Frameworks"
    de la documentación de secure.py.

--- colabora/app.py
+++ colabora/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, Response
+import secure
 
+secure_headers = secure.Secure()
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("defaults")
 app.config.from_envvar("COLABORA_CONFIG", silent=True)
+
+@app.after_request
+def set_secure_headers(response):
+    secure_headers.framework.flask(response)
+    return response

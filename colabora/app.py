e4bbc967fe05daec7be94d2a00d30f976bacefb2
Patricio Paez <pp@pp.com.mx>
2023-12-31 13:43:22 -0600

    Remover información del servidor

--- colabora/app.py
+++ colabora/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response
 import secure
 
-secure_headers = secure.Secure()
+secure_headers = secure.Secure(server=secure.Server())
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("defaults")

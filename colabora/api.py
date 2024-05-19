bb0fccb606a952b87c12d8f160bacc32794ec222
Patricio Paez <pp@pp.com.mx>
2024-05-18 19:09:27 -0600

    Iniciar API
    Crear un blueprint, las vistas login e iniciativa, y
    el decorador key_required.

---
+++ colabora/api.py
@@ -0,0 +1,50 @@
+import functools
+from werkzeug.security import check_password_hash
+from flask import Blueprint
+from flask import request
+from flask import abort
+from flask import current_app
+from .db import get_db
+from .db import agrega_iniciativa
+
+
+bp = Blueprint('api', __name__, url_prefix='/api')
+
+
+def key_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if request.json.get('key') != current_app.config['API_KEY']:
+            abort(403)
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.route('/login', methods=['POST'])
+def login():
+    json = request.json
+    user = json['username']
+    password = json['password']
+    if check_password_hash(current_app.config['API_PASSWORD'], password):
+        return {'result': 'ok: ingreso correcto',
+                'key': current_app.config['API_KEY']}
+    else:
+        return {'result': 'error: ingreso denegado'}
+
+
+@bp.route('/iniciativa', methods=['POST'])
+@key_required
+def iniciativa():
+    db = get_db()
+    json = request.json
+    entidad = json['entidad']
+    legislatura = json['legislatura']
+    numero = json['numero']
+    documento = json['documento']
+    cambios = json['cambios']
+    result = agrega_iniciativa(db, entidad, legislatura,
+                               numero, cambios, documento,
+                               tema="", resumen="", comentario="")
+    return {'result': result}
---
+++ tests/test_api.py
@@ -0,0 +1,84 @@
+from pytest import raises
+from werkzeug.exceptions import Forbidden
+from colabora.main import app
+from colabora.api import key_required
+
+
+def test_login_ok(client):
+    response = client.post('/api/login',
+                           json={'username': 'usuario_api',
+                                 'password': 'contrasena_api'})
+    assert response.status == '200 OK'
+    assert response.json == {'result': 'ok: ingreso correcto',
+                             'key': 'api-key-cambiar'}
+
+
+def test_login_error(client):
+    response = client.post('/api/login',
+                           json={'username': 'usuario_api',
+                                 'password': 'contrasena'})
+    assert response.status == '200 OK'
+    assert response.json == {'result': 'error: ingreso denegado'}
+
+
+def test_key_required_key_correcto(client):
+
+    def view():
+        return 'Ok'
+    with app.test_request_context(json={'key': 'api-key-cambiar'}):
+        wrapped = key_required(view)
+        assert wrapped() == 'Ok'
+
+
+def test_key_required_key_incorrecto(client):
+
+    def view():
+        return 'Ok'
+    with app.test_request_context(json={'key': 'api-key-incorrecto'}):
+        wrapped = key_required(view)
+        with raises(Forbidden):
+            assert wrapped()
+
+
+def test_key_required_no_key(client):
+
+    def view():
+        return 'Ok'
+    with app.test_request_context(json={}):
+        wrapped = key_required(view)
+        with raises(Forbidden):
+            assert wrapped()
+
+
+def test_iniciativa_agregar_sin_key(client):
+    response = client.post('/api/iniciativa',
+                           json={'entidad': 'entidad1',
+                                 'legislatura': 'legislatura1',
+                                 'numero': 4,
+                                 'cambios': 'cambios4',
+                                 'documento': 'documento4'})
+    assert response.status == '403 FORBIDDEN'
+
+
+def test_iniciativa_agregar_ok(client):
+    response = client.post('/api/iniciativa',
+                           json={'entidad': 'entidad1',
+                                 'legislatura': 'legislatura1',
+                                 'numero': 4,
+                                 'cambios': 'cambios4',
+                                 'documento': 'documento4',
+                                 'key': 'api-key-cambiar'})
+    assert response.status == '200 OK'
+    assert response.json == {'result': 'ok: iniciativa 4 creada'}
+
+
+def test_iniciativa_agregar_error(client):
+    response = client.post('/api/iniciativa',
+                           json={'entidad': 'entidad1',
+                                 'legislatura': 'legislatura1',
+                                 'numero': 3,
+                                 'cambios': 'cambios3',
+                                 'documento': 'documento3',
+                                 'key': 'api-key-cambiar'})
+    assert response.status == '200 OK'
+    assert response.json == {'result': 'error: iniciativa 3 no creada'}

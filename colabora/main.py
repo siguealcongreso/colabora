bd7034c9af5bf006da3bf49d6ed8ed7f1f9d50cf
Patricio Paez <pp@pp.com.mx>
2024-05-18 19:10:01 -0600

    Agregar valores para API
    Y se registra la blueprint en la aplicación.

--- colabora/main.py
+++ colabora/main.py
@@ -1,3 +1,7 @@
 from .app import app
 
 from .views import *
+
+from . import api
+
+app.register_blueprint(api.bp)
--- defaults.py
+++ defaults.py
@@ -1,2 +1,5 @@
 DATABASE = "colabora.sqlite"
 SECRET_KEY = "cambiar-esta"
+API_USER = "api-user-cambiar"
+API_PASSWORD = "scrypt:32768:8:1$fACm8mOZ3DiuiaJv$f45af8ff2ad1ba5b27756c18cbafe40ec7f6638f05f37c347da7b215477af7bdbb2794385a9af720a941fa0e3b826805325709d64727980c3ac38f373486cda2"
+API_KEY = "api-key-cambiar"

2ca0d50ad6a0d5bd57edd0f69d60e0589b104931
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-08-13 11:53:12 -0600

    Se agregó el spellchecker en español

--- colabora/util.py
+++ colabora/util.py
@@ -1,15 +1,20 @@
 import re
-
+from spellchecker import SpellChecker
 def revisa_tema(tema): 
     correcciones = []
     estado = 'Ok'
+    
+    spell = SpellChecker(language='es')
+    incorrectas = spell.unknown(re.findall(r'\b\w+\b', tema))
+
+    if incorrectas:
+        correcciones.append(f'Revisar si la(s) palabra(s) "{", ".join(incorrectas)}" están bien escritas')
     if tema[-1] == '.':
         correcciones.append('El tema no debe llevar punto al final')
-    if tema[0] or tema[-1] == ' ':
+    if tema[0] == ' ' or tema[-1] == ' ':
         correcciones.append('El tema no debe llevar espacio ni al inicio ni al final')
     if re.search(r"(?<!\.)\n", tema):
         correcciones.append('El tema no debe llevar un renglón nuevo sin que vaya después de un punto')
-    # TO-DO: Agregar spell checker en español
 
     if len(correcciones) != 0:
         estado = 'Error'
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,1 +1,26 @@
+from colabora.util import revisa_tema
 
+def test_revisa_tema_ortografia():
+    tema = 'Saldud'
+    result = revisa_tema(tema)
+    assert result == ('Error', ['Revisar si la(s) palabra(s) "saldud" están bien escritas'])
+
+def test_revisa_tema_punto_final():
+    tema = 'Salud.'
+    result = revisa_tema(tema)
+    assert result == ('Error', ['El tema no debe llevar punto al final'])
+
+def test_revisa_tema_espacio():
+    tema = ' Salud'
+    result = revisa_tema(tema)
+    assert result == ('Error', ['El tema no debe llevar espacio ni al inicio ni al final'])
+
+def test_revisa_tema_punto_renglon():
+    tema = 'Salud\nSalud'
+    result = revisa_tema(tema)
+    assert result == ('Error', ['El tema no debe llevar un renglón nuevo sin que vaya después de un punto'])
+
+def test_revisa_tema_correcto():
+    tema = 'Salud'
+    result = revisa_tema(tema)
+    assert result == ('Ok', [])

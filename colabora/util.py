a4fea02f1f8ce647379a0d1b4bef7c748019a153
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-08-12 20:43:17 -0600

    Se añadieron pruebas para util.py

--- colabora/util.py
+++ colabora/util.py
@@ -5,7 +5,7 @@ def revisa_tema(tema):
     estado = 'Ok'
     if tema[-1] == '.':
         correcciones.append('El tema no debe llevar punto al final')
-    if tema[0] or tema[-1] == ' ':
+    if tema[0] == ' ' or tema[-1] == ' ':
         correcciones.append('El tema no debe llevar espacio ni al inicio ni al final')
     if re.search(r"(?<!\.)\n", tema):
         correcciones.append('El tema no debe llevar un renglón nuevo sin que vaya después de un punto')
@@ -13,5 +13,5 @@ def revisa_tema(tema):
 
     if len(correcciones) != 0:
         estado = 'Error'
-        
+
     return estado, correcciones

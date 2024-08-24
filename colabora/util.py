0249329c4a2156f96d63e06bb922e43166a7b153
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-08-23 18:16:38 -0600

    Se corrigió la función revisa_tema para apropiadamente revisar el inicio y fin

--- colabora/util.py
+++ colabora/util.py
@@ -9,9 +9,9 @@ def revisa_tema(tema):
 
     if incorrectas:
         correcciones.append(f'Revisar si la(s) palabra(s) "{", ".join(incorrectas)}" están bien escritas')
-    if tema[-1] == '.':
+    if tema.endswith== '.':
         correcciones.append('El tema no debe llevar punto al final')
-    if tema[0] == ' ' or tema[-1] == ' ':
+    if tema.startswith == ' ' or tema.endswith == ' ':
         correcciones.append('El tema no debe llevar espacio ni al inicio ni al final')
     if re.search(r"(?<!\.)\n", tema):
         correcciones.append('El tema no debe llevar un renglón nuevo sin que vaya después de un punto')

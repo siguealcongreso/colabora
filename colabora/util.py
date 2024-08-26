41b7d1ad58ffba6cab700fa87672563a31b0ed19
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-08-26 16:42:10 -0600

    correccion de util.py

--- colabora/util.py
+++ colabora/util.py
@@ -9,14 +9,15 @@ def revisa_tema(tema):
 
     if incorrectas:
         correcciones.append(f'Revisar si la(s) palabra(s) "{", ".join(incorrectas)}" están bien escritas')
-    if tema[-1] == '.':
-        correcciones.append('El tema no debe llevar punto al final')
-    if tema[0] == ' ' or tema[-1] == ' ':
-        correcciones.append('El tema no debe llevar espacio ni al inicio ni al final')
-    if re.search(r"(?<!\.)\n", tema):
-        correcciones.append('El tema no debe llevar un renglón nuevo sin que vaya después de un punto')
+    if tema:
+        if tema[-1] == '.':
+            correcciones.append('El tema no debe llevar punto al final')
+        if tema[0] == ' ' or tema[-1] == ' ':
+            correcciones.append('El tema no debe llevar espacio ni al inicio ni al final')
+        if re.search(r"(?<!\.)\n", tema):
+            correcciones.append('El tema no debe llevar un renglón nuevo sin que vaya después de un punto')
 
-    if len(correcciones) != 0:
-        estado = 'Error'
+        if len(correcciones) != 0:
+            estado = 'Error'
         
     return estado, correcciones
